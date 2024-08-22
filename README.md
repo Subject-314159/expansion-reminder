aaf2b75172a6dd66a2e76e2ef98d1f24e0a11b21
Subject-31415 <168562337+Subject-314159@users.noreply.github.com>
2024-08-22 20:19:45 +0200

    v1.2.0

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
+# Expansion & FFF reminder
+
 Feeling lonely? Here's a companion mod that reminds you that the expansion is close but at the same time so far away
 
 ![reminder](https://raw.githubusercontent.com/Subject-314159/expansion-reminder/main/assets/reminder-screenshot.png)
 
 ![reminder-detailed](https://raw.githubusercontent.com/Subject-314159/expansion-reminder/main/assets/detailed-reminder.png)
+
+Compatible with [Thought bubbles](https://mods.factorio.com/mod/thought-bubbles)
+
+---
+
+# Remote interface
+
+You can now retrieve random FFFs via the remote interface. Simply require this mod in your info.json!
+
+Remote interface available to retrieve random FFF: `local fact = remote.call("friday-facts", "get-random-fact")`
+
+Return structure:
+
+```lua
+local fact = {
+    date = "Mmm DD YYYY",
+    fff = "FFF #123",
+    title = "Blog post title",
+    detail = { -- Optional, data.detail not always present
+        name = "Name",
+        summary = "Short description",
+        description = "What it is" -- Optional, data.detail.description not always present
+    }
+}
+```
 
 ---
 
@@ -10,4 +37,4 @@ Feeling lonely? Here's a companion mod that reminds you that the expansion is cl
 
 -   Implement check for base version and installed mods
 -   Add separate remarks for when Factorio 2'ish is installed
--   Add interface to programmatically retrieve FFFs
+-   Update locale for after the release date
--- control.lua
+++ control.lua
@@ -2,6 +2,53 @@ local facts = require("lib.facts")
 local DEFAULT_TICKS_PER_DAY = 25000
 local MIN_TICKS_PER_DAY = 600
 
+local get_random_fact = function()
+
+    -- Get FFF blog info
+    local k = math.random(1, #facts)
+    -- Init return array
+    local data = {
+        fff = facts[k].fff,
+        date = facts[k].date,
+        title = facts[k].title
+    }
+
+    -- Add random in-depth content (if available)
+    if facts[k].content then
+        -- Make content type array
+        local cat = {}
+        for type, c in pairs(facts[k].content) do
+            table.insert(cat, type)
+        end
+
+        -- Get random content type
+        local rnd = math.random(1, #cat)
+        local rcat = cat[rnd]
+        local content = facts[k].content[rcat]
+
+        -- Get random description
+        local description = content[math.random(1, #content)]
+        data.detail = {
+            name = description.name or nil,
+            summary = description.summary or nil,
+            description = description.description or nil
+        }
+    end
+
+    return data
+end
+-- Return data structure
+-- local data = {
+--     date = "Mmm DD YYYY",
+--     fff = "FFF #123",
+--     title = "Blog post title",
+--     detail = { -- Optional, data.detail not always present
+--         name = "Name",
+--         summary = "Short description",
+--         description = "What it is" -- Optional, data.detail.description not always present
+--     }
+-- }
+
 local remind = function()
     global.days_without_expansion = (global.days_without_expansion or 0) + 1
     if settings.global["er_remind-days"].value then
@@ -16,26 +63,12 @@ local remind = function()
     end
 
     if settings.global["er_remind-facts"].value then
+        local fact = get_random_fact()
+
         local j = math.random(1, 3)
-        local k = math.random(1, #facts)
-        game.print({"expansion-reminder.fact-" .. j, facts[k].fff, facts[k].date, facts[k].title})
-
-        -- Show random in-depth content (if available)
-        if facts[k].content then
-            -- Make content type array
-            local cat = {}
-            for type, c in pairs(facts[k].content) do
-                table.insert(cat, type)
-            end
-
-            -- Get random content type
-            local rnd = math.random(1, #cat)
-            local rcat = cat[rnd]
-            local content = facts[k].content[rcat]
-
-            -- Get random description
-            local description = content[math.random(1, #content)]
-            game.print(description.summary)
+        game.print({"expansion-reminder.fact-" .. j, fact.fff, fact.date, fact.title})
+        if fact.detail then
+            game.print(fact.detail.summary)
         end
     end
 end
@@ -97,4 +130,25 @@ script.on_event(defines.events.on_tick, function(e)
     else
         manual_tick_update()
     end
+end)
+
+local register_remote_interfaces = function()
+    -- Remove our old interface if exists
+    if remote.interfaces["friday-facts"] then
+        remote.remove_interface("friday-facts")
+    end
+
+    -- Add our interface again
+    local functions = {
+        ["get-random-fact"] = get_random_fact
+    }
+    remote.add_interface("friday-facts", functions)
+end
+
+script.on_init(function(e)
+    register_remote_interfaces()
+end)
+
+script.on_load(function(e)
+    register_remote_interfaces()
 end)
